d7fce3eadb8ac5249cd47bfd07f513006e738356
xiaoxudan <xyz8808@qq.com>
2023-10-16 14:36:42 +0800

    添加项目文件。

---
+++ AkamaiSensorDataDecode/Program.cs
@@ -0,0 +1,42 @@
+namespace AkamaiSensorDataDecode
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = "2;3619142;3491397;2,10,0,0,1,16;r_T E@9[%kOE$.phR6Z{k*f+(fXW8`pf~-$O%!&e#qQA:B(}5]$PWV0ieMgc!/FO[VV%ht|Hel=lCJ6>5}JSZ;v>H?_^*(Qg(wxD*7K,IP3CBK=J59hD]4u!,{D%L)bl.!%aKSzE)&y92GDHF;tcr}eLX^ dbzT^,-6ya+c$acDgb/)357!_?J,w (Oh~F`Toz2zr}P4v%&9<Yi) #texl9w?Nt1l&70[DoMC7S8(3C_j%=gd!ZH|C;9KZ#3w.Dj4Jjf^$X=.ANu)]l2@A<E5k%-Pp=rN)W,=ZB87yGA.DO#<e{S[|q{Fw+x@aHjS4xK*Tmuu[UUt>Wv>w;k$I@C#VL^-E+aY44a=n07GuY`q.Lq6 L}6VN}@|,.g0|IO0![SqJYbL+G B]:`aI[i[Ymr#=;{`[&tIlXHyNatZH13-]AqX~o[4a!eRhIj&7vmZYRDtUd,hV| {-fA62|R_={j^xl~]N/;.}PR>CU*|g,9s>*db?Mf3Wh^R&N5bh]asOA9~]C.N9%:!w&kcKR@Vg[HCw;%wuh %905hb.|ZP{F*[X;E*,[@o*6;5`jzrAqWx$IA*T4/BZ-![[MX68tj,V_r=nxZqdgwZYb_V#gipQ}g<MEyn<.%GsO?iM$kR`#7QSxfr9 5F{J74?*A/D5/^5CxBstv2;s}DQrxvR.NhCwvcyh)$;5,]@MpR<I:mQtU!>alnODYv]19!H}5fxgxN>^|_HXf4LkA8OEOoO8_xgJUKsu;6K_%L2fqCNi:&:7@9,31IG5Q6lh>.W>(#<G?Ct>F 9G>h5b,Y}Od#vs<eiPdw.Br(Haf`P~G>?ZzUnV5kG 8]PJr0)kg[!@x9cc~yuL((-Gd=mY-z[=LoxO++)$MQ%BO2oStH>{}]b*uZ2M,|%5W7auQX`c.B:,z?l$% RjG|./YhGi*E$8b0AJ-z}!OpG$U+T{DU_>)BM)MAq}PS?;X<AS=|9@{;yZ0nAn:FgD69b8fXJE4?wyL,@Q@0LZ2]<sh3sC*8SF,PmFx)><^0VM,q4NF3>jFe[:#_kW4Kq%w;cF#3&||9}s_;-9x&?]KvsD9${O!Enmr-_5$#y4+=.;*w;mcf(L+pqHG19bEk;C;o.+  !-4xXKH=d7@7j)$Z[x#57%a+]_s=g[R]Fq[YTTPFyWfmFb_9H;?+=}c<lx/_8,09S| @H`RR4k~,a@.x r}{/ou=z2FvVWPt#E[$8XTM3v-:tQM=~Fhfu0[8HZK#losDN#@_q/7I-o;F*amUts49H9=,o(C-s`]HhB^*Hz*8!c D+w!s00P^!wzeNr4%v,;m$PvUpOLhXMk1&xz(:HqRj7F=-Znp<N&h|]%n:hr8-&GtlWkq=S{/H^zdN/RE>iwJIj+BCw3UXR)T?9X`6Gnc<=f(xK%*|U];2g(oSW}b/x=%~n4Dy3u([p1<#aNN?x8}TF~`H&Pju(cFDCb|P}Y%}HhesOR<I<;L{ VQ6{8s$7 Zh_.LzR3b;X4$@ YqrM[Q62q7j^pC^sL6Pn}J2r9mvo<c.H8V`>sbtN/@!vQEBaA-G[N@zI~ Y#?UiVJ_+k;A(,YgNKF IgXDeR4bJC;|l9p7I>,Yu`ID*2532:oYyJ2{pG8KSLg|jyNeh$Rd;|`dj)=K5;9O$<=P1h/nW2.f)2C[zuejd$aEPbJ$)<]i*HP=EcrIt?A3(hd YKW9_V9KUwy8>Zo88txX,Vp/6O]EQd+XYh=I,Po-+__y%D5k?~6iCtB,)+;^cGLxUjz/5c<!(+hPnbZ%,U[z{a$d%r57N7%g:|sW=9Ao|Q`#H^lp]6^rV3%)5J)1mbi:[Fl[FHIO81PWlgoz<$=fJ,i`L[uy24fIhX#=ZAwu4s9: P]yi^ x9xU3M}}(KC-L(R(%`$?h5s3h0c&>4/$Gk3>!l  *H]k^c$ELDfvItf;i:{0TJ<uIH*QZ0:f(`cllrDnwm4P/hUxgCw9aY4q0;D|-c-]pHV$opX -3c*a3ygI)`|3Zvh|&-FR.c/[.LfimE0@DpByZb1+eJyTYeB24?a ,NW0g2SaiJ7>J0QAw}dg;&L7OX7{B>r3qE%RB]85JgK3f1XS+@!$bR(y)K,o/;h?%P=!0VSkx8p}:)]C44,:},U4NnnZy#lbTNl5nBq9QOJ[&ZFL.H:B+1F.Gr=_VA}PP#J~)V2H|aug)Cx)y61D9=!p/aiZ,A(fXO=,xT6b60&SyvW_[%};19-sI#*)JcW09UVgqPB^8=JuHC,2]aW*(;~DMv#5[i%jx&nM+j//ksHU`[rfm~8,c|lnL+0fzI6[+%hX;LCSP,y1b8YG%Q6%6 ~ 8Gw1vVSD?CSV[VE$jpLzh]e%ALTGT!y,d#|~qQS7Ybpm4p%hBauf@V[2;r?fvGHWG(OoX8KCQb~,G;J$;8RA)B]y,zAt g6Jbe{>Ax-)MRz(QC`Q.a|{p)$e2f*k3jv?{ Gua sv<G;.EQfODj?7;RjBbK!n,d#I>=g,N%9<l.KnDQjTN-UdUy4k:3`TrVq37EtRPJ^iBZ,S#>GSJ*Ynl:L3uW?(RDk0,5y )Kp#oV;&bm@6H}Vnn?jU}=EB`R$L#*lSFLNr`se>t;2C{nGWgM]/zo{wg2ZWgpCU04`wJ+m#%^@LaL.QY$c}_oNGukM{(DI~BGrD5eP!h6h~$;w&9uUUFK94%%d3aLEUu381{[RSMry6|A8bt1KGXi1@61q[pD^TJ<L%hEoxzw3JiXS^Ne usVM,,-o.|T!Ge0mR?).mb:);|tq6aX<PMUcxoqhS$dWX](9DMe>&Q.|<oszJvq]sp3-[Qg[Q X]kGVP7=~-{(u[+hkfT$(v1/P`|5E-?HwLg7rbJIOQH^BAlCPnDv*z8?spFV{llI~ET1ajRajpjvkiFq1G2stj4Sx3Rm<=1#kx3}]aQe^|}*.4tOf)|Xoi#lx5b$NW`S7]td:BCTY*%@3kTUW/liZ9Z3`/j*_Zle(Q1-p !GAe|E<C9{IrmU|&ThQt]&Ji*xjBbQ?]b!.Zd64R2,_%$u5S!N4aKtC_ ruJvpju uq.Oz)H:(]038ZsFvc?)U2VfupBu~z(Hr.k2Qc$q Y8/9Xyb:%cK&b(OagFv#y=T4I*F!l8^i=|+3X2jM7tqW=d/;@5_ttCj=UA`>`-1<!gu~V}hZS.~h[2ag~dYZm=U>6}q{%D_{L@IeIxdFF2<pyBy*J>&9E~S/`Nzl;gl-(v.IxaN?B7: yyGGqZEo(Z|u(nzg@|AF5%^~a>/zBw-/4^Bj01&/Y6jU?9mP:y4JDEj8xYaZ&+b8Lv|stv1XpV)#brH<S3+yyJKm5z)x76KRZ:(_&@8e%dBL-=yzSFgC@HwL3,;GMXy|6pkO K9Q)n[XSSU%avMz/<x?D<DWz~9Md`S$Wg$N/3 ,zcZ(05P5_rS090B[t.X=Xv~X8LY:toXonwl,P==K@R5{%(6M#>ep.Kg8~9#xn<06=vv-3/eG`|jR.;&H{:fuP: C.>mT.02NKou<-@{j5X7!5Dr9n|_/DLx_Po)rpxi3@]f48<m66HL<[d6V>:wU>GjODpAgT$,Uc5Blm}zs5_hQt}W&n9x>t6PELuJ70HV~*stIUqke-RUZ#1pHX5EtYtG@7byo>[b7T.@r~E3DLzMSxE|PF}h7,Kn|sbih-TpPbGc:XrrzN97I#6)_R1xYGu>@T8# (SaHnOg:i<>J*p)+f%(QK*QsY,hs.iOdc6Y-dY$d}PImN;^d=rcL}j-s`<q.J:{.CpD~U9<cvbg-lZEicO>6$x$ooIu3dA<QiDqa4!h]zUo9)};O9,)v#dslX4/Mug^rG @Cue=D|TY{%fdr^PcTETcA->#K&|cGK6[Qun<A,MmH>Rvj_b5y(:P7^.;/6j*XtJE;|D!bBqlafy:B*l8xXB%Xr8GouyTpvm<5HT&BzAoQ}L@:~O&Cfe[i+LOV~fA6G84TF*J8)bb29B<UuoPwX[{MvA*,X)>{gY[6-H3hST&Q~_=tM1VvW]fD`=R_(RnKbvaU~,.fUR;spS{;S-4g9KfIn?#huQTNTg|C]u0H#Rq%g<V!77Nos$V}5c{Xlfh,+}t~Rj~#^H#$<7KS`T5gZFu_G,hqg)-7E+tK6/?WTx&zYX&YOo!gZPCpi-Yx9fHJ<-wE~sNR-+ -8AJZ%2nCOj{l#Kn(wkNn{U])&r4U=ni0G0.L@gY52h?tw(cRVGJaEAW^EelsqDKVz[~%Ctvok#xC1ZOf36[J:~)wL,5!p5^A_09^<$Xfjr/O,Y^ dH$.jzO]1iZPi/3L,pDI]+9avN]x;zh|1!^p&c}g:,RCjF6dK=Xs`6{slerNMy[=F0~AR}YCg_?(_3DdPOo.pIG097D61cP}68&J]@{e=;ef>r2LL?g%DlXNbamanPBl 4!SfI!#[c53cSxG-9h3#eba!~D4I!w>nosBbL(}nvZL< Gs zW}qems,/@RRFE~SWO3XI *x}hkWAfGWW5bUQZZD6j[t[eAIN.k<&rtk$mvg^5ed%UE?5TR|,Pe~$%X.O_Beq25WpixZc@y2?&ids0Ce&KUq{-`%@R`59!Ly-Mc[cb*1c>%(4UwFc9Hu~&hUh*xZ^Tjx8?SGK6$LYM8KH~>o9m*Zh#h]r6|r~w.>`p?)Bwp:RO=Zp=h9`97,D]QKq9}9,~pa*8[^}[c!ME2`c<fF~[#a<>2*O-{FT3/Dxa$5yA-g,./WkUvd+rH1<k_bCepj++]P%c7Eqh[7jj~AkIp`=}ZLi1(9loK;c~YjGqDQz#~V78UxD9bT/(fCm>EM2rv|HQ?cId>Q1%7tRbpO#aA>}uZQgLLhSaARD7p?XQg&)9K%d+7tFDjt]`H?nTVpRL-C<_M~k=oFmXF2}4SY^b;CAu$$P>KmSwts^$tH8pW)^!.!%8XEuz[lbt^T1{Fi[Ln&a0/c_8,VCV~li(M7&%w,x. 1nczFB?_#g5;$qHS~e!OPTvF5|/z00ACLF/Sw./InX40t ^&K)WTO#T*tZrx/6ZLdSMxq`L/a@,-mxkyGGaE$||YYM_gx2Ycn^j,r~)R/&jfbH8m_G$UgsH0~$AJq.DFa@?&U.UO$1&?<e*I70N_y&mLNGpsLVUt42A$$FcKksuj8nk@6B{Rq-|[tp.Sv,q-<F^H/OqM,;{jIdr.}$e<s1#`u(ApBh3a%,F/[t1/fA@^h,4SKR]u<OJ4kA3UQmT2/HKGY:<(l69_}@Ntm,wl<pXYsy})a0s?~.H?8g.YT{O`sX`3-MHEmAE:SwUse4(K-;kehDlgj#9axVkIBb`Y1oml@_@ke1$L:X x)b>9GXrD1Lb3TkuEx}A<q!sNK%tT:d*7D il_.N%P2W!PKCsdJWb6XB| `Y5}M%0Q(n! ^yWsg2v0^kt`{*cT tLQ=9xm> 5J6x,El9pD*bNxMSt[Efx_4}orfgWF|^vG7$>I XDys;vU~1h]AVydEC|E$8y&jDT$ti%<$X?%rPJ?%XTi=&w>}*!13A);*`0eGSw9s:__a/7(1.H}o<;HbVhlh|,HE3Qt8Z4H-y=.%^W*s##Q!?YX3y?1(SK[>>g!3,.I<N{qH,^Q<+1hI!UQsP J ICbTHT`?v~{uV_W]QVp;fuRb3Crg!-Uc,cVxz,i{_d`kq  </7*.@!65Jioqw_gpQNE.C}Y&nL3wKHJm^ylV+x|bzbz!7&IX-oCG;9<qzGQSVh`|)T<K-i#mJ [Oa$_uNw8;VJ:U(cU`[rtHEij|Oww,&l*6s9o6`(_z.p}>TNDRQ&0]_+xB* Iq`P$7k&d3r=s6MG;d=,(AHqvKd68NAB_:4:XrDy)8r;&9jah8ZiX}&ScGe8LXYC}Si_+N(Tg)oDxLpmoWx @Go4L!W`p5GT>k(xSoS-*q`2i4kv!`49!:5X#g$k{J#&1v%6l1~WR=0nZ(gd~iCRR)t[E9lA^31s`M^:MH?K#[mClaq i`{UW51rFZ-EarH`H:)?ROt=azVr06)=ZO6M7Bi}X{PkYD}eYSy(oEI>A3E&)eTm1%pVZ(ZPx$^N>9EtJ#Oy`:>1D9H=K}l8WQRn2tF?yy[S(s1gZM|D;|z|/6OHX<,D$|{CjN1)ow_P9t>osrH#rHbd&*<;>3*h-:Av++^kRVTL4&>v!(Y3* ?0Q]K(;yIBz~.#o@}TGB LRE OTbW=NATKAu@DVc#o4GJsG.S*L^<L(_Rbv1xRVTf}Tg&6fbiD";
+            var a = new SensorDataDecoder(data);
+            var result=a.Decode();
+            foreach ( var e in result )
+            {
+                Console.WriteLine($"{e}") ;
+            }
+
+            return;
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
---
+++ AkamaiSensorDataDecode/SensorDataDecoder.cs
@@ -0,0 +1,116 @@
+﻿namespace AkamaiSensorDataDecode
+{
+    public class SensorDataDecoder
+    {
+        string data;
+        int step1_seed;
+        int step2_seed;
+        public SensorDataDecoder(string data)
+        {
+            this.data = data;
+            var tmp = data.Split(';');
+            step1_seed = Convert.ToInt32(tmp[2]);
+            step2_seed = Convert.ToInt32(tmp[1]);
+            this.data = string.Join(';', tmp.Skip(4));
+        }
+
+        public string[] Decode()
+        {
+            var arr = data.ToCharArray();
+            var dict2 = MakeStep2(step2_seed);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var c = arr[i];
+                foreach (var item in dict2)
+                {
+                    if (item.Value[i] == c)
+                    //var index = item.Value.FindIndex(o => o == arr[i]);
+                    //if (index >= 0)
+                    {
+                        arr[i] = item.Key;
+                    }
+                }
+
+            }
+            var a = string.Join("", arr);
+            var data_arr = a.Split(',');
+            var dict1 = MakeStep1(data_arr,step1_seed);
+            var r = dict1.ToList();
+            r.Reverse();
+            foreach (var item in r)
+            {
+                var Oqf = data_arr[item.Item1];
+                data_arr[item.Item1] = data_arr[item.Item2];
+                data_arr[item.Item2] = Oqf;
+            }
+            var aa = string.Join(",", data_arr);
+            var first2 = aa.IndexOf(",2,");
+            var splitstr = aa.Substring(1, first2);
+            var d = aa.Split(splitstr).Skip(4).ToArray();
+            return d;
+        }
+        List<(int,int)> MakeStep1(string[] arr, int step1_seed)
+        {
+            var result= new List<(int, int)>();
+            var seed = (uint)step1_seed;
+            int vqf;
+            int Dqf;
+            string Oqf;
+            var len = arr.Length;
+            for (var index = 0; index < len; index++)
+            {
+                vqf = (int)(seed >> 8 & 65535) % len;
+                seed *= 65793;
+                seed &= 4294967295;
+                seed += 4282663;
+                Dqf = (int)((seed &= 8388607) >> 8 & 65535) % len;
+                seed *= 65793;
+                seed &= 4294967295;
+                seed += 4282663;
+                seed &= 8388607;
+                //if(!result.ContainsKey(vqf))
+                result.Add((vqf, Dqf));
+            }
+
+            return result;
+        }
+
+        Dictionary<char,List<char>> MakeStep2(int step2_seed)
+        {
+            var result=new System.Collections.Concurrent.ConcurrentDictionary<char, List<char>>();
+            var all_char = " !#$%&()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+            Parallel.ForEach(all_char, new ParallelOptions() { MaxDegreeOfParallelism = Environment.ProcessorCount }, c =>
+            {
+                var str = "".PadRight(data.Length, c);
+                var seed = (uint)step2_seed;
+                var r = new List<char>();
+                var arr = new long[] { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, -1, 2, 3, 4, 5, -1, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, -1, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91 };
+
+                for (var index = 0; index < str.Length; index++)
+                {
+                    var current = str[index];
+                    var JEp = seed >> 8 & 65535;
+                    seed *= 65793;
+                    seed &= 4294967295;
+                    seed += 4282663;
+                    seed &= 8388607;
+                    var DEp = arr[str[index]];
+                    var nEp = (int)str[index];
+                    if (nEp >= 32 && nEp < 127)
+                        DEp = arr[nEp];
+
+                    if (DEp >= 0)
+                    {
+                        DEp += JEp % all_char.Length;
+                        DEp %= all_char.Length;
+                        current = all_char[(int)DEp];
+                        r.Add(current);
+                    }
+                }
+                result.TryAdd(c, r);
+            });
+           
+            return result.ToDictionary(o=>o.Key,v=>v.Value);
+        }
+    }
+}
